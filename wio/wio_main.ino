716a23fff7856a4f1cd0d0d80e578d46d2a1f385
shunmame <32508729+Boss-matsunaga@users.noreply.github.com>
2019-09-25 17:46:44 +0900

    Update wio_main.ino

--- wio/wio_main.ino
+++ wio/wio_main.ino
@@ -1,15 +1,16 @@
 #include <TinyGPS++.h>
 #include <WioLTEforArduino.h>
 #include <stdio.h>
-//#include <MsTimer2.h>
+#include <time.h>
 
 #define BUZZER_PIN (WIOLTE_D38)
 #define BUTTON_PIN (WIOLTE_A6)
-#define INTERVAL        (60000)
 #define RECEIVE_TIMEOUT (10000)
+#define REGULAR_TIME (60) //not ms.
 
 TinyGPSPlus gps;
 WioLTE Wio;
+
 
 char data[100];
 char url[200];
@@ -18,6 +19,9 @@ char GpsData[100];
 char GpsDataLength;
 char incoming;
 int flag=0;
+struct tm now;
+time_t saver=0;
+
 
 void GpsBegin(HardwareSerial* serial)
 {
@@ -30,13 +34,13 @@ void GpsCheck() {
   incoming = GpsSerial->read();
   gps.encode(incoming);
  // if (gps.location.isUpdated()) {
-        sprintf(url, "http://machimori.japanwest.cloudapp.azure.com/wio?lat=32.476392&lon=130.607272&parent_ID=P10000&buzzer_num=B10000&flag=%d",flag);
+        sprintf(url, "http://machimori.japanwest.cloudapp.azure.com/wio?lat=%.5f&lon=%.5f&parent_ID=P10000&buzzer_num=B10000&flag=%d",gps.location.lat(),gps.location.lng(),flag);
         SerialUSB.println(gps.location.lat());
         SerialUSB.println(gps.location.lng());
         SerialUSB.println(url);
-//  }else{
-//    SerialUSB.println(###ERROR.notGPS###);
-//  }
+ // }else{
+ //   SerialUSB.println(###ERROR.notGPS###);
+ // }
   
   
   int r = Wio.HttpGet(url, data,sizeof(data),6000);
@@ -45,15 +49,34 @@ void GpsCheck() {
   SerialUSB.println(data);
   if(r==1){
     digitalWrite(BUZZER_PIN,HIGH);
+    delay(1000);
+    digitalWrite(BUZZER_PIN,LOW);
   }
   SerialUSB.println("wio-end");
+  delay(500);
 }
 
-
+///////////////////////////////////////////////////////////
+bool getWioTime(){
+  time_t timer;
+  if (!Wio.GetTime(&now)) {
+    SerialUSB.println("### ERROR! ###");
+    return false;
+  }else{
+    timer=mktime(&now);
+    if(timer-saver>=REGULAR_TIME){
+      saver=timer;
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+//////////////////////////////////////////////////////
 
 void setup() {
   SerialUSB.println("");
-  SerialUSB.println("--- START ---------------------------------------------------");
   GpsBegin(&Serial);
   Wio.Init();
   Wio.PowerSupplyLTE(true);
@@ -63,15 +86,20 @@ void setup() {
   pinMode(BUTTON_PIN,INPUT);
   Wio.TurnOnOrReset();
   Wio.Activate("soracom.io", "sora", "sora");
-  delay(500);
+  delay(5000);
   SerialUSB.println("setup-end");
-  //MsTimer2::set(INTERVAL,GpsCheck);
 }
 
 
 void loop() {
-  int i = digitalRead(BUTTON_PIN);
-  if(i == HIGH){ 
+  if(getWioTime()){
+    flag = 0;
+    GpsCheck();
+    SerialUSB.println("OK");
+    SerialUSB.println(saver);
+  }
+  if(digitalRead(BUTTON_PIN) == HIGH){ 
+    flag = 1;
     digitalWrite(BUZZER_PIN,HIGH);
     Wio.LedSetRGB(0,255,0);
     delay(500);
