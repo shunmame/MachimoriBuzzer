14d4784e8ba5beb49cc271a6c8005cf85c8dea63
shunmame <32508729+Boss-matsunaga@users.noreply.github.com>
2019-10-12 09:33:26 +0900

    Update wio_main.ino

--- wio/wio_main.ino
+++ wio/wio_main.ino
@@ -2,70 +2,72 @@
 #include <WioLTEforArduino.h>
 #include <stdio.h>
 #include <time.h>
-
 #define BUZZER_PIN (WIOLTE_D38)
 #define BUTTON_PIN (WIOLTE_A6)
 #define RECEIVE_TIMEOUT (10000)
-#define REGULAR_TIME (60) //not ms.
-
+#define GPS_OVERFLOW_STRING "OVERFLOW"
+#define DEFAULT_TIME (1200) //unit: s, not ms.
 TinyGPSPlus gps;
 WioLTE Wio;
-
-
+int RegularTime = DEFAULT_TIME;
 char data[100];
 char url[200];
 HardwareSerial* GpsSerial;
 char GpsData[100];
 char GpsDataLength;
 char incoming;
-int flag=0;
+int flag = 0;
 struct tm now;
-time_t saver=0;
-
-
-void GpsBegin(HardwareSerial* serial)
-{
+time_t saver = 0;
+int r = 0;
+double gpslat;
+double gpslng;
+int himo=0;
+//////////////////////////////////////////
+void GpsBegin(HardwareSerial* serial){
   GpsSerial = serial;
   GpsSerial->begin(9600);
   GpsDataLength = 0;
 }
-
-void GpsCheck() {
-  incoming = GpsSerial->read();
-  gps.encode(incoming);
- // if (gps.location.isUpdated()) {
-        sprintf(url, "http://machimori.japanwest.cloudapp.azure.com/wio?lat=%.5f&lon=%.5f&parent_ID=P10000&buzzer_num=B10000&flag=%d",gps.location.lat(),gps.location.lng(),flag);
-        SerialUSB.println(gps.location.lat());
-        SerialUSB.println(gps.location.lng());
-        SerialUSB.println(url);
- // }else{
- //   SerialUSB.println(###ERROR.notGPS###);
- // }
-  
-  
-  int r = Wio.HttpGet(url, data,sizeof(data),6000);
-  SerialUSB.println(r);
-  SerialUSB.println("END");
-  SerialUSB.println(data);
-  if(r==1){
-    digitalWrite(BUZZER_PIN,HIGH);
-    delay(1000);
-    digitalWrite(BUZZER_PIN,LOW);
+const char* GpsRead(){
+  while (GpsSerial->available()) {
+    incoming = GpsSerial->read();
+    // SerialUSB.print(incoming);
+    gps.encode(incoming);
+    if (incoming == '\r') continue;
+    if (incoming == '\n') {
+      GpsData[GpsDataLength] = '\0';
+      GpsDataLength = 0;
+      if (gps.location.isUpdated()) {
+        gpslat = gps.location.lat();
+        gpslng = gps.location.lng();
+        if(gpslat < -90 || gpslat > 90) continue;
+        if(gpslng < -180 || gpslng > 180) continue;
+        sprintf(data, "http://machimori.japanwest.cloudapp.azure.com/wio?lat=%.5f&lon=%.5f&parent_ID=P10000&buzzer_num=B10000&flag=%d", gpslat, gpslng, flag);
+        return data;
+      }
+    }
+    if (GpsDataLength > sizeof (GpsData) - 1) { // Overflow
+      GpsDataLength = 0;
+    }
+    GpsData[GpsDataLength++] = incoming;
   }
-  SerialUSB.println("wio-end");
-  delay(500);
+  return NULL;
 }
-
+  
+  
+  
+  
 ///////////////////////////////////////////////////////////
 bool getWioTime(){
   time_t timer;
   if (!Wio.GetTime(&now)) {
-    SerialUSB.println("### ERROR! ###");
     return false;
   }else{
     timer=mktime(&now);
-    if(timer-saver>=REGULAR_TIME){
-      saver=timer;
+    if(timer-saver>=RegularTime){
+      saver = timer;
+      RegularTime = DEFAULT_TIME;
       return true;
     }
     else{
@@ -74,39 +76,62 @@ bool getWioTime(){
   }
 }
 //////////////////////////////////////////////////////
-
 void setup() {
   SerialUSB.println("");
-  GpsBegin(&Serial);
   Wio.Init();
   Wio.PowerSupplyLTE(true);
   Wio.PowerSupplyGrove(true);
   delay(500);
-  pinMode(BUZZER_PIN,OUTPUT);
-  pinMode(BUTTON_PIN,INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
   Wio.TurnOnOrReset();
+  GpsBegin(&Serial);
   Wio.Activate("soracom.io", "sora", "sora");
   delay(5000);
   SerialUSB.println("setup-end");
 }
-
-
 void loop() {
+  const char* gpsdata;
   if(getWioTime()){
     flag = 0;
-    GpsCheck();
-    SerialUSB.println("OK");
+    gpsdata = GpsRead();
+    while(gpsdata == NULL){
+      gpsdata = GpsRead();
+    }
+    SerialUSB.println(gpsdata);
+    r = Wio.HttpGet(gpsdata, data,sizeof(data), 6000);
+    SerialUSB.println(r);  
     SerialUSB.println(saver);
   }
-  if(digitalRead(BUTTON_PIN) == HIGH){ 
+  if(digitalRead(BUTTON_PIN) == HIGH && himo == 0){ 
+    digitalWrite(BUZZER_PIN, HIGH);
     flag = 1;
-    digitalWrite(BUZZER_PIN,HIGH);
-    Wio.LedSetRGB(0,255,0);
-    delay(500);
-    digitalWrite(BUZZER_PIN,LOW);
-    delay(100);
-    GpsCheck();
+    himo = 1;
+    gpsdata = GpsRead();
+    while(gpsdata == NULL){
+      gpsdata = GpsRead();
+    }
+    SerialUSB.println(gpsdata);
+    int r = Wio.HttpGet(gpsdata, data, sizeof(data), 6000); 
+    SerialUSB.println(r);  
+    SerialUSB.println("OK");
+    
   }else{                                                                                                                                                                                                                                                                                                                                                                     
-    Wio.LedSetRGB(255,0,0);
+    Wio.LedSetRGB(255, 0, 0); 
+  }
+    if(digitalRead(BUTTON_PIN) == HIGH && himo == 1){
+      digitalWrite(BUZZER_PIN,LOW);
+      himo = 0;
+      delay(1000);
+    }
+  if(r == 1){
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(2000);
+    digitalWrite(BUZZER_PIN, LOW);
+    r = 0;
+  }
+  else if(r == 3){
+    RegularTime = 60;
+    r = 0;
   }
 }
